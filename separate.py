cbc98e139e09be2120d5c9eb6056f7692c15904b
hedun <edunhassan1006@gmail.com>
2026-07-31 14:49:17 +0100

    feat: Added dynamic naming in saved output

--- separate.py
+++ separate.py
@@ -1,8 +1,11 @@
+import os
 from demucs.api import Separator 
 
 
 
 def extract_vocals(audio_file):
+    if not os.path.exists(audio_file):
+        raise FileNotFoundError(f"Input File Not Found: {audio_file}")
     separator = Separator()  # capital S = the class; () creates an instance with default settings
     origin, separated = separator.separate_audio_file(audio_file)
     # separated is a dict: {"vocals": tensor, "drums": tensor, "bass": tensor, "other": tensor}
@@ -15,5 +18,6 @@ if __name__ == "__main__":
 
     audio_file = sys.argv[1]  # takes filename from command line, e.g. python separate.py test.mp3
     vocals, samplerate = extract_vocals(audio_file)
-    save_audio(vocals, "vocals_output.wav", samplerate)
-    print("Saved vocals to vocals_output.wav")
+    name = os.path.splitext(os.path.basename(audio_file))[0]
+    save_audio(vocals, name+"_Vocals.wav", samplerate)
+    print(f"Saved vocals to: {name}_Vocals.wav")
