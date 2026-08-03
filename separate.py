1f2a89b298b75e67a13ea81372837a65aac99b33
hedun <edunhassan1006@gmail.com>
2026-08-03 16:07:49 +0100

    feat: support vocal separation for video files
    * Add  subprocess to extract audio from videos
    * Add extension validation for audio and video formats
    * Handle unsupported file types gracefully

--- separate.py
+++ separate.py
@@ -12,14 +12,40 @@ def extract_vocals(audio_file):
     vocals = separated["vocals"]
     return vocals, separator.samplerate
 
+import subprocess
+
+def extract_audio_from_video(video_file, audio_output):
+    subprocess.run([
+        "ffmpeg", "-i", video_file,
+        "-vn", "-acodec", "pcm_s16le", "-ar", "44100",
+        audio_output
+    ])
+    return audio_output
+
 if __name__ == "__main__":
     import sys
     from demucs.api import save_audio
     if len(sys.argv) < 2:
         print("Usage: python3 separate.py <audio_file>")
         sys.exit(1)
-    audio_file = sys.argv[1]  # takes filename from command line, e.g. python separate.py test.mp3
-    vocals, samplerate = extract_vocals(audio_file)
-    name = os.path.splitext(os.path.basename(audio_file))[0]
-    save_audio(vocals, name+"_Vocals.wav", samplerate)
-    print(f"Saved vocals to: {name}_Vocals.wav")
+    
+    
+    input_file = sys.argv[1]  # takes filename from command line, e.g. python separate.py test.mp3
+
+    if os.path.splitext(os.path.basename(input_file))[1] in [".mp3", ".wav",".flac", ".m4a", ".ogg"]:
+        vocals, samplerate = extract_vocals(input_file)
+        name = os.path.splitext(os.path.basename(input_file))[0]
+        save_audio(vocals, name+"_Vocals.wav", samplerate)
+        print(f"Saved vocals to: {name}_Vocals.wav")
+
+    # Check if the file is a video
+    elif os.path.splitext(os.path.basename(input_file))[1] in [".mp4",".avi","mkv","webm"]:
+        vidname = os.path.splitext(os.path.basename(input_file))[0]
+        audio_output = vidname + "_Audio.wav"
+        output = extract_audio_from_video(input_file, audio_output)
+        vocals,samplerate = extract_vocals(output)
+        output_name = vidname + "_vocals.wav"
+        save_audio(vocals,output_name,samplerate)
+        print(f"saved vocals as {output_name}")
+    else:
+        print("NOT A VIDEO/AUDIO FILE")
