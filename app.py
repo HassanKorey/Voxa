d05d154e370bac1ce68dda859e4707a6e2eb7e51
hedun <edunhassan1006@gmail.com>
2026-08-07 11:04:38 +0100

    feat: Flask web app with upload form, downloads, and cleanup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, render_template
 from separate import extract_vocals, extract_audio_from_video, add_audio_to_video, save_audio
 import os
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Vocal isolation API is running"
+    return render_template("index.html")
 
 @app.route("/separate", methods=["POST"])
 def separate():
@@ -17,19 +17,26 @@ def separate():
     uploaded_file.save(uploaded_file.filename)
     if os.path.splitext(os.path.basename(uploaded_file.filename))[1] in [".mp3", ".wav",".flac", ".m4a", ".ogg"]:
         audio_vocals, samplerate = extract_vocals(uploaded_file.filename)
-        vocals_output_name = uploaded_file.filename + "_vocals.wav"
+        name = os.path.splitext(os.path.basename(uploaded_file.filename))[0]
+        vocals_output_name = name + "_vocals.wav"
         save_audio(audio_vocals,vocals_output_name , samplerate)
-        return send_file(vocals_output_name, as_attachment=True)
+        response = send_file(vocals_output_name, as_attachment=True)
+        os.remove(vocals_output_name)
+        os.remove(uploaded_file.filename)
+        return response
     elif os.path.splitext(os.path.basename(uploaded_file.filename))[1] in [".mp4",".avi",".mkv",".webm"]:
         vidname = os.path.splitext(os.path.basename(uploaded_file.filename))[0]
         audio_output = vidname + ".wav"
         video_audio = extract_audio_from_video(uploaded_file.filename, audio_output)
         video_vocals, samplerate = extract_vocals(video_audio)
-        vocals_output_name = uploaded_file.filename + "_vocals.wav"
+        vocals_output_name = vidname + "_vocals.wav"
         save_audio(video_vocals, vocals_output_name, samplerate)
         os.remove(audio_output)
         video_output = add_audio_to_video(uploaded_file.filename, vocals_output_name, vidname + "_vocals.mp4")
-        return send_file(video_output, as_attachment=True)
-
+        response = send_file(video_output, as_attachment=True)
+        os.remove(vocals_output_name)
+        os.remove(video_output)
+        os.remove(uploaded_file.filename)
+        return response
 if __name__ == "__main__":
     app.run(debug=True)
